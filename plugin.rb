ab109900017af7304ab3c87805bb3c634ebe1077
Public Happiness Andy <focallocal.london@gmail.com>
2025-10-31 18:40:35 +0000

    Revert "update"

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: docuss
 # about: Docuss plugin for Discourse
-# version: 1.0.36-all-fixes
+# version: 1.0.35-root-cause-fix
 # authors: Sylvain Quendez
 # url: https://github.com/sylque/dcs-discourse-plugin
 
